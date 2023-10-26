ba9b28602689605ae1815f8c74f7218b7ff59310
mush42 <musharraf.omer@gmail.com>
2023-10-26 16:05:01 +0200

    import psutil from a temp dir during add-on uninstall.

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -4,32 +4,36 @@
 # This file is covered by the GNU General Public License.
 
 
+import contextlib
 import os
+import shutil
 import sys
+import tempfile
 
 from logHandler import log
 
 
 _DIR = os.path.abspath(os.path.dirname(__file__))
-_LIB_DIR = os.path.join(_DIR, "synthDrivers", "piper_neural_voices", "lib")
-BIN_DIR = os.path.join(_DIR, "synthDrivers", "piper_neural_voices", "bin")
-sys.path.insert(0, _LIB_DIR)
-import psutil
-sys.path.remove(_LIB_DIR)
-del _DIR, _LIB_DIR
+_PIPER_SYNTH_DIR = os.path.join(_DIR, "synthDrivers", "piper_neural_voices")
+LIB_DIR = os.path.join(_PIPER_SYNTH_DIR, "lib")
+BIN_DIR = os.path.join(_PIPER_SYNTH_DIR, "bin")
+del _DIR, _PIPER_SYNTH_DIR
 
 
-def force_kill_piper_grpc_server():
+def onUninstall():
+    with _temporary_import_psutil() as psutil:
+        force_kill_piper_grpc_server(psutil)
+
+
+def force_kill_piper_grpc_server(psutil):
     log.debug("Trying to force kill GRPC server process")
     grpc_server_processes = list(filter(
-        lambda p: "piper-grpc" in p.name,
+        lambda p: "piper-grpc" in p.name().lower(),
         psutil.process_iter(attrs=["name", "exe"])
     ))
-    grpc_server_exe = os.path.normpath(
-        os.path.abspath(os.path.join(BIN_DIR, "piper-grpc.exe"))
-    )
+    grpc_server_exe = os.path.join(BIN_DIR, "piper-grpc.exe")
     for proc in grpc_server_processes:
-        if os.path.normpath(os.path.abspath(proc.exe())) == grpc_server_exe:
+        if os.path.samefile(proc.exe(), grpc_server_exe):
             proc.kill()
             log.debug(f"Killed process with pid {proc.pid}")
     psutil.wait_procs(
@@ -38,5 +42,17 @@ def force_kill_piper_grpc_server():
     )
 
 
-def onUninstall():
-    force_kill_piper_grpc_server()
+@contextlib.contextmanager
+def _temporary_import_psutil():
+    temp_import_dir = tempfile.TemporaryDirectory()
+    src = os.path.join(LIB_DIR, "psutil")
+    py3_lib_src = os.path.join(LIB_DIR, "python3.dll")
+    dst = os.path.join(temp_import_dir.name, "psutil")
+    shutil.copytree(src, dst)
+    shutil.copy2(py3_lib_src, dst)
+    sys.path.insert(0, temp_import_dir.name)
+    import psutil
+    yield psutil
+    sys.path.remove(temp_import_dir.name)
+    with contextlib.suppress(Exception):
+        temp_import_dir.cleanup()
